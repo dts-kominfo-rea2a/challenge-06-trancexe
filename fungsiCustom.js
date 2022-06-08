0f092c215e42e7f2579e43ead5300df9767ab1c3
timotheus ingga pranata <trancexe@gmail.com>
2022-06-08 14:55:52 +0900

    Update fungsiCustom.js
    penambahan fungsi bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
+
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let hasil = [];
+  // read file1 utf-8, if not err parsed to second word
+  fs.readFile(file1, "utf-8", (err, data) => {
+    if (err) return fnCallback(err, null);
+    hasil.push(JSON.parse(data).message.split(' ')[1])
+    // read file2 utf-8, if not err parsed to second word
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) return fnCallback(err, null);
+      hasil.push(JSON.parse(data)[0].message.split(' ')[1])
+      // read file3 utf-8, if not err parsed to second word
+      fs.readFile(file3, "utf-8", (err, data) => {
+        if (err) return fnCallback(err, null);
+        hasil.push(JSON.parse(data)[0].data.message.split(' ')[1])
+        // return array of data
+        fnCallback(null, hasil);
+      });
+    });
+  }); 
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
